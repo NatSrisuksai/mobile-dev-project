97347a3205fc13ef66a238326b3abd73c83e80a7
PakaponRattanasrisuk <pakapon.paw@gmail.com>
2024-11-02 18:27:56 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -12,6 +12,15 @@ import CameraScreen from './components/cameraScreen/CameraScreen';
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBMaBVY2HXa1eEL0Skc98VfbJidu12TnRo",
+  authDomain: "social-media-app-4ff33.firebaseapp.com",
+  databaseURL: "https://social-media-app-4ff33-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "social-media-app-4ff33",
+  storageBucket: "social-media-app-4ff33.firebasestorage.app",
+  messagingSenderId: "651977666525",
+  appId: "1:651977666525:web:5831e2054b7fb9a6821034"
+};
 const TabNavigator = () => (
   <Tab.Navigator initialRouteName="Home">
     <Tab.Screen name="Home" component={HomeScreen} />
